bf0ac1c19f0d6b77393286034ad09a5cc568e28b
bambinek-doc <dominik.cholewski@outlook.com>
2021-05-25 01:03:30 +0100

    even more projects

--- ca-javascript/project-2-beat-mix-start/presetHandler.js
+++ ca-javascript/project-2-beat-mix-start/presetHandler.js
@@ -2,7 +2,17 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (req, index, newPresetArray) => {
+    if (index > 15) {
+        return ['404', "Not Found"];
+    } else if (res === "GET") {
+        return `['200', ${}]`
+    } else if () {
+
+    } else {
+        return [400, "Bad Request"]
+    }
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
