7d22576da783628228bf3958b76375f1e12d768e
dom-techwriter <dominik@belvo.com>
2021-06-02 12:06:30 +0100

    soome older projects

---
+++ ca-javascript/whale-talk.js
@@ -0,0 +1,21 @@
+let input = "Domeniku";
+let resultArray = [];
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+        if (input[i] === vowels[j]){
+            resultArray.push(input[i]);
+        };
+    }
+    if (input[i] === "e") {
+        resultArray.push(input[i]);
+        };
+
+    if (input[i] === "u") {
+        resultArray.push(input[i]);
+        };
+};
+
+console.log(resultArray.join('').toUpperCase());
+
